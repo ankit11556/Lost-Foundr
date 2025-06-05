fc8c5d0ad444e1f9a9e2edd6bd20ac15af5525d2
ankit <saharan442090@gmail.com>
2025-06-05 16:18:01 +0530

    add icon & update postCard

--- frontend/src/components/CommentButton.jsx
+++ frontend/src/components/CommentButton.jsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 import { FaRegComment } from "react-icons/fa6";
 const CommentButton = () =>{
   const [boxShow,setBoxShow] = useState(false)
+
   const handleClick = () =>{
-    setBoxShow(true)
+   setBoxShow(prev => !prev);
   }
   return(
     <>
-    <div>
-      <button className="hover:cursor-pointer hover:text-blue-500 "
+    <div className="flex flex-col items-center gap-2 w-[100px]">
+      <button className="hover:cursor-pointer hover:text-blue-500  "
       onClick={handleClick}
       ><FaRegComment /></button>
+
       {boxShow &&
-      <div>
-        <input type="text" placeholder="comment" />
+      <div className=" ">
+        <input type="text" placeholder="comment" className="border px-2 py-1 rounded"/>
         <button>Add</button>
       </div>
       }
--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,36 +1,36 @@
 import LikeButton from "./LikeButton"
 import CommentButton from "./CommentButton"
+import { CiLocationOn } from "react-icons/ci";
+import { IoWalletOutline } from "react-icons/io5";
+import { CiUser } from "react-icons/ci";
 const PostCard = ({post})=>{
   return(
-  <div className="bg-slate-900 text-white rounded-xl shadow p-5 w-full max-w-[300px] transition-all hover:scale-105 flex flex-col item-between gap-3">
-  <p className="text-slate-200">
-    Title: <span className="text-white font-semibold">{post.title}</span>
-  </p>
-  <p className="text-slate-200">Status: 
-     <span className={`font-semibold text-white  px-1 py-1 rounded-full text-sm
-      ${post.status === 'lost'? 'bg-red-600':'bg-green-600'}`}>
-        {post.status}
+  <div className="bg-slate-700 text-white flex flex-col max-w-xs md:max-w-md lg:max-w-lg mx-auto px-8 py-4 rounded-2xl w-full h-auto">
+  <div className="flex  w-full justify-between py-2 ">
+    <span className="text-3xl font-semibold">{post.title}</span>
     
-    </span>
-  </p>
-  <p className="text-slate-200">
-    Item Name: <span className="text-white font-semibold">{post.itemName}</span>
-  </p>
-  <p className="text-slate-200">
-    Date: <span className="text-white font-semibold">{new Date (post.date).toLocaleDateString()}</span>
-  </p>
-  <p className="text-slate-200">
-    Location: <span className="text-white font-semibold">{post.location}</span>
-  </p>
-  <p className="text-slate-200">
-    Posted By: <span className="text-white font-semibold">{post.postedBy}</span>
-  </p>
-  <p className="text-slate-200">
-    Contact: <span className="text-white font-semibold">{post.contactInfo}</span>
-  </p>
-  <div className="flex justify-between">
-    <LikeButton></LikeButton>
-    <CommentButton></CommentButton>
+    <span className={`text-base  px-4 py-1.5 rounded-xl
+    ${post.status=='lost'?'bg-red-500':'bg-green-500'}`}>
+    {post.status}</span>
+  </div>
+  <div className="flex justify-around w-full py-2 text-lg">
+  <div className="flex flex-col w-full ">
+ <div className="flex items-center gap-1">
+  <IoWalletOutline />
+   <span>{post.itemName}</span>
+ </div>
+<div className="flex items-center gap-1 ">
+  <CiLocationOn /> <span>{post.location}</span>
+</div>
+  <div className="flex items-center gap-1">
+    <CiUser />
+    <span>{post.postedBy}</span>
+  </div>
+  </div>
+  <div className="flex flex-col gap-1">
+    <span>{new Date(post.date).toLocaleDateString()}</span>
+    <span>{post.contactInfo}</span>
+  </div>
   </div>
   </div>
   )
