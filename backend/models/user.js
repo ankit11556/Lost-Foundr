c75b6f8625eb9e5c1d1a2b1e033924ab46a94ca8
ankit <saharan442090@gmail.com>
2025-06-13 14:48:10 +0530

    change arrow func to simple func in  user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt') 
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,7 +19,27 @@ const userSchema = new mongoose.Schema({
   }
 },{timestamps: true})
 
+userSchema.pre('save', async function(next){
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
 
+userSchema.methods.isValidPassword = async function(password){
+  try {
+    return await bcrypt.compare(password, this.password)
+  } catch (error) {
+    throw new Error("Password comparision failed");
+    
+  }
+}
 
 const User = mongoose.model('User',userSchema)
 
