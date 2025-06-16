2a87bcc24e5acb17e87b3669a33340d29db5d3c6
ankit <saharan442090@gmail.com>
2025-06-16 11:51:37 +0530

    generate access token & refresh token

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+const generateToken = (userId)=>{
+  try {
+    const accessToken = jwt.sign(
+      {userId},
+      process.env.ACCESS_TOKEN_KEY,
+      {expiresIn: "15m"}
+    );
+
+    const refreshToken = jwt.sign(
+      {userId},
+      process.env.REFRESH_TOKEN_KEY,
+      {expiresIn: "7d"}
+    );
+
+   return {accessToken,refreshToken}
+  } catch (error) {
+     console.error("Error generating token:",error);
+    throw new Error("Token generated failed")
+  }
+}
+
+module.exports = generateToken
