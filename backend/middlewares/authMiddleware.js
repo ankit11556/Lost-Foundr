6eb2fc6ca906e4433dbeafa1c05ea0fc0bce60d7
ankit <saharan442090@gmail.com>
2025-06-18 15:18:24 +0530

    add atuh middleware & handle protect routes

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user')
+
+const protectRoutes =async (req,res,next) =>{
+  const token = req.cookies.access_token ||
+  (req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer") &&
+    req.headers.authorization.split(" ")[1]
+  );
+
+  //check json web token exists & is verified
+  if(!token){
+    return res.status(403).json({message: "Not authorized, token missing"})
+  }
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token,process.env.ACCESS_TOKEN_KEY);
+  } catch (error) {
+    if(error.name === "TokenExpiredError"){
+      return res.status(401).json({message: "Session expired. Please login again"})
+    };
+    return res.status(401).json({message: "Invalid token. Please login."})
+  }
+
+  const user = await User.findById(decoded.userId).select('-password');
+  if(!user){
+    return res.status(401).json({message: "User not found. Please sign in."})
+  }
+  req.user = user
+
+  next()
+}
+
+module.exports = {protectRoutes}
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {createPost,getPosts} = require('../controllers/postController')
 const upload = require('../middlewares/imgUpload')
+const {protectRoutes} = require('../middlewares/authMiddleware')
 
-router.post("/add",upload.single('image'),createPost)
+router.post("/add",protectRoutes,upload.single('image'),createPost)
 router.get("/",getPosts)
 module.exports = router
