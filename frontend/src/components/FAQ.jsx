7c07fa534b1e26916d03f8f25c20382cedd16f04
ankit <saharan442090@gmail.com>
2025-07-28 16:59:00 +0530

    create FQA section

---
+++ frontend/src/components/FAQ.jsx
@@ -0,0 +1,19 @@
+function FAQ() {
+  return (
+    <section className="py-10 bg-gray-100 mt-8">
+      <h2 className="text-3xl font-bold text-center mb-6">FAQs</h2>
+      <div className="max-w-2xl mx-auto space-y-4">
+        <div>
+          <h3 className="font-semibold">❓ What if I lost my ID?</h3>
+          <p className="text-gray-600">Post it with all details and wait for someone to find it.</p>
+        </div>
+        <div>
+          <h3 className="font-semibold">❓ Can I edit my post?</h3>
+          <p className="text-gray-600">Yes. Use your dashboard or contact support.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FAQ
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import HeroSection from "../components/HeroSection"
 import HowItWorks from "../components/HowItWorks"
 import RecentPosts from "../components/RecentPosts"
 import TrustSection from "../components/Trust & Privacy"
+import FAQ from "../components/FAQ"
 const HomePage = () =>{
   return(
 <>
@@ -9,6 +10,7 @@ const HomePage = () =>{
 <HowItWorks></HowItWorks>
 <RecentPosts></RecentPosts>
 <TrustSection></TrustSection>
+<FAQ></FAQ>
 </>
   )
 }
