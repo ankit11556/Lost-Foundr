0bdf8f74d07ade73bb5813f8bc9cc6377a37b1e4
ankit <saharan442090@gmail.com>
2025-07-28 16:55:29 +0530

    create trust & privacy component

---
+++ frontend/src/components/Trust & Privacy.jsx
@@ -0,0 +1,13 @@
+const TrustSection = () =>{
+  return(
+    <section className="bg-blue-50 py-10 text-center mt-8">
+      <h2 className="text-2xl font-bold mb-4">Trust & Privacy</h2>
+      <p className="max-w-xl mx-auto">
+        We don’t store your sensitive information. You can delete your posts anytime.
+        Our platform is safe and secure for connecting with others.
+      </p>
+    </section>
+  )
+}
+
+export default TrustSection
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "../components/HeroSection"
 import HowItWorks from "../components/HowItWorks"
 import RecentPosts from "../components/RecentPosts"
+import TrustSection from "../components/Trust & Privacy"
 const HomePage = () =>{
   return(
 <>
 <HeroSection></HeroSection>
 <HowItWorks></HowItWorks>
 <RecentPosts></RecentPosts>
+<TrustSection></TrustSection>
 </>
   )
 }
