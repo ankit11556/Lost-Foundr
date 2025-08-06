a14a46b86569ee5138484ab3558bd48dcad75a3f
ankit <saharan442090@gmail.com>
2025-08-06 17:53:49 +0530

    Improve DB connection with strictQuery and safe exit on failure

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery',true)
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDb connected successfully");  
   } catch (error) {
     console.log("MongoDb not connected",error);
+    process.exit(1)
   }
 }
 
