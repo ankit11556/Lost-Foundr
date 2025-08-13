5207eaaf4fc475676d647c7945a3ad9fe755af01
ankit <saharan442090@gmail.com>
2025-08-13 19:38:36 +0530

    add loading in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import AppRoutes from "./routes/AppRoutes"
-function App() {
-  
-  return (
-  
-  <div className="  bg-gradient-to-r from-blue-50 to-indigo-100  min-h-screen w-full">
-    <AppRoutes></AppRoutes>
-  </div>
+import AppRoutes from "./routes/AppRoutes";
+import { useAuth } from "./contexts/AuthContext";
 
-    
-  )
+function App() {
+  const { loading } = useAuth();
+
+  if (loading) {
+    // Auth check complete hone tak loading show karo
+    return <div className="min-h-screen flex justify-center items-center">
+      <p>Loading...</p>
+    </div>;
+  }
+
+  return (
+    <div className="bg-gradient-to-r from-blue-50 to-indigo-100 min-h-screen w-full">
+      <AppRoutes />
+    </div>
+  );
 }
 
-export default App
+export default App;
