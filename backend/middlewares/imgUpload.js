e36a94914ec09a04d344363d00a0f1037cc2dfd5
ankit <saharan442090@gmail.com>
2025-08-14 12:49:41 +0530

    feat: integrate Cloudinary for image uploads

--- backend/middlewares/imgUpload.js
+++ backend/middlewares/imgUpload.js
@@ -1,14 +1,16 @@
 const multer = require('multer');
-const storage  = multer.diskStorage({
-  destination: function (req,file,cb) {
-    cb(null,'uploads/');
-  },
-  filename: function (req,file,cb) {
-    const uniqueName = Date.now() + '--' + file.originalname;
-    cb(null,uniqueName)
-  }
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinary')
+
+const storage  = new CloudinaryStorage({
+  cloudinary: cloudinary,
+params: {
+  folder: "my_uploads",
+  allowed_formats: ['jpg', 'png', 'jpeg', 'webp']
+}
+
 })
 
-const upload = multer({storage})
+const upload = multer({storage: storage})
 
 module.exports = upload
