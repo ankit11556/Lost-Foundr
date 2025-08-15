201ca7c181b832545b732a6c10e41bb10595e858
ankit <saharan442090@gmail.com>
2025-08-15 12:46:51 +0530

    change rederirect uri

--- backend/utils/googleConfig.js
+++ backend/utils/googleConfig.js
@@ -7,5 +7,5 @@ const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 exports.oauth2client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-   GOOGLE_REDIRECT_URI
+  GOOGLE_REDIRECT_URI 
 )
