d2bffe31c5f7f5b8f41048240ef1d38838e1a1b9
ankit <saharan442090@gmail.com>
2025-08-16 13:45:53 +0530

    feat: add spinner loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,17 @@ function App() {
   const { loading } = useAuth();
 
   if (loading) {
-    // Auth check complete hone tak loading show karo
-    return <div className="min-h-screen flex justify-center items-center">
-      <p>Loading...</p>
-    </div>;
+    
+    return( <div className="min-h-screen flex justify-center items-center bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 text-white">
+      {/* Spinner */}
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-white mb-6"></div>
+
+      {/* Message */}
+      <p className="text-xl font-semibold tracking-wide animate-pulse">
+        Our App is starting...
+        </p>
+    </div>
+    )
   }
 
   return (
